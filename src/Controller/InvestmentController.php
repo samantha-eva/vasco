b21f7f14ceb2a729c6bb82d45fc9d31f6ab3a2cc
samantha-eva <samanthathiebaut@gmail.com>
2025-05-07 18:18:38 +0200

    Afficher la liste des operations afin qu'il puisse investir si il le souhaite

---
+++ src/Controller/InvestmentController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Operation;
+use App\Entity\Investment;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class InvestmentController extends AbstractController
+{
+    #[Route('/investment', name: 'investment_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $operationId = $request->request->get('operation_id');
+        $montant = $request->request->get('montant');
+
+        $operation = $em->getRepository(Operation::class)->find($operationId);
+        $user = $this->getUser();
+
+        $now = new \DateTime(); // mutable
+
+        $investment = new Investment();
+        $investment->setUser($user);
+        $investment->setOperation($operation);
+        $investment->setMontant((float)$montant);
+        $investment->setDate($now);
+
+
+        $em->persist($investment);
+        $em->flush();
+
+        $this->addFlash('success', 'Investissement enregistré !');
+        return $this->redirectToRoute('app_operation');
+    }
+}
